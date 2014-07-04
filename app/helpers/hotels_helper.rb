ebb5ec30bf66bcc3f242fde894690d2a816b43b9
Yevgeniy Chaikin <nutaksho@gmail.com>
2014-07-04 05:01:08 +0300

    Add top five hotels on the landing page

--- app/helpers/hotels_helper.rb
+++ app/helpers/hotels_helper.rb
@@ -8,4 +8,8 @@ module HotelsHelper
     current_user.grades.where("hotel_id = ?", params[:id]).any?
   end
 
+  def top_five
+    Grade.all.group(:hotel_id).average(:value).sort_by { |h_id, avg| avg }.last(5)
+  end
+
 end
