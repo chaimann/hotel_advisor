0812ed96ce6216446bef5a1d725f53bb03441ef1
Yevgeniy Chaikin <nutaksho@gmail.com>
2014-07-06 04:52:42 +0300

    Add hotels controller spec

--- spec/controllers/hotels_controller_spec.rb
+++ spec/controllers/hotels_controller_spec.rb
@@ -1,8 +1,40 @@
 require 'spec_helper'
 require 'rails_helper'
 
-# describe HotelsController do
-#   let(:user) { FactoryGirl.create(:user) }
-#   let(:hotel) { FactoryGirl.create(:hotel) }
+describe HotelsController do
+  let(:user) { FactoryGirl.create(:user) }
 
-# end
+  describe "when user is not signed in" do
+    describe "GET index" do
+      it "redirects to sign in page" do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe "GET new" do
+      it "redirects to sign in page" do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
+  describe "when user is signed in" do
+    before { sign_in user }
+
+    describe "GET index" do
+      it "renders index page" do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+
+    describe "GET new" do
+      it "renders new hotel's template" do
+        get :new
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+end
